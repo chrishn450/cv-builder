74cb5eecd6cb2c96a071694d47823210fadb5760
chrishn450 <christopherhoelnilsen@gmail.com>
2026-02-17 22:12:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@
   const t = (key) => (CVI.t ? CVI.t(state.ui.lang || "en", key) : key);
 
   const preview = qs("preview");
+  const printBtn = qs("printBtn");
   const downloadPdfBtn = qs("downloadPdfBtn");
   const downloadHtmlBtn = qs("downloadHtmlBtn");
 
