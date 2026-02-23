6ad3ef5c751fc51bc566f8387653f3ebde1205de
chrishn450 <christopherhoelnilsen@gmail.com>
2026-02-23 18:34:08 +0100

    Create manifest.js

---
+++ templates/manifest.js
@@ -0,0 +1,14 @@
+// templates/manifest.js
+(function () {
+  // Legg til nye templates her, ferdig.
+  window.CV_TEMPLATES = [
+    {
+      id: "nurse",
+      name: "Nurse (Classic)",
+      css: "/templates/nurse/template.css",
+      render: "/templates/nurse/render.js",
+    },
+    // Eksempel:
+    // { id: "modern", name: "Modern", css: "/templates/modern/template.css", render: "/templates/modern/render.js" },
+  ];
+})();
