8ddc6127ac71a91735b9ccc04cfbc5bb0d7cf3b6
chrishn450 <christopherhoelnilsen@gmail.com>
2026-02-26 21:22:10 +0100

    Delete api/logout.js

--- api/logout.js
+++
@@ -1,7 +0,0 @@
-import { json, setCookie } from "./_utils.js";
-
-export default async function handler(req, res) {
-  if (req.method !== "POST") return json(res, 405, { error: "Method not allowed" });
-  setCookie(res, "cv_session", "", { maxAge: 0, path: "/" });
-  return json(res, 200, { ok: true });
-}
